b24b991cf41fe00d2562d98f00d129ddb7a13b87
Braden Currah <bacurrah@csuchico.edu>
2025-10-08 12:42:04 -0700

    Update icon on change

--- src/widgets/Shortcut.tsx
+++ src/widgets/Shortcut.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Widget from "../Widget";
 import { LinkIcon } from "@phosphor-icons/react";
 import styles from "./Shortcut.css";
 
 export default function Shortcut({ url = "" }: { url?: string }) {
   const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [url]);
 
   return (
     <Widget size={{ width: 1, height: 1 }} resizeable={false}>
