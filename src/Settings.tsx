9e1d1f6f4a8acb780e57ade10d4f1b09be108b5f
Bogachan Karabiyik <bogacizmir@gmail.com>
2025-12-01 12:17:41 -0800

    Delete src directory

--- src/Settings.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const SettingsContext = createContext<Settings>(null);
-
-interface Settings {}
-
-interface ThemeSettings {}
-
-interface TemplateSettings {}
